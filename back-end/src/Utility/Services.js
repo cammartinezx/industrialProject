ad45c8ecc8dca989e1cbd93fb88c798d7a24aa7b
Camila <camila.martinez2002@gmail.com>
2025-03-05 17:48:47 -0600

    updated services

--- back-end/src/Utility/Services.js
+++ back-end/src/Utility/Services.js
@@ -1,7 +1,11 @@
 const UserPersistence = require('../Persistence/UserPersistence');
+const InstructorPersistence = require('../Persistence/InstructorPersistence');
+const StudentPersistence = require('../Persistence/StudentPersistence');
 
 class Services {
     static #user_persistence = null;
+    static #instructor_persistence = null;
+    static #student_persistence = null;
 
     static get_user_persistence() {
         if (this.#user_persistence === null) {
@@ -9,6 +13,19 @@ class Services {
         }
         return this.#user_persistence;
     }
+    static get_instructor_persistence() {
+        if (this.#instructor_persistence === null) {
+            this.#instructor_persistence = new InstructorPersistence();
+        }
+        return this.#instructor_persistence;
+    }
+    static get_student_persistence() {
+        if (this.#student_persistence === null) {
+            this.#student_persistence = new StudentPersistence();
+        }
+        return this.#student_persistence;
+    }
+   
 }
 
 module.exports = Services;
