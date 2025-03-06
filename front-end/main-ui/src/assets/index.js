8803c2af009f3083cc3c69bb9f9fad39e2558424
Camila <camila.martinez2002@gmail.com>
2025-03-06 11:33:28 -0600

    added symbols

--- front-end/main-ui/src/assets/index.js
+++ front-end/main-ui/src/assets/index.js
@@ -1,7 +1,7 @@
-import brainwave from "./brainwave.svg";
+import edunova from "./edunova.svg";
 import check from "./check.svg";
-import brainwaveSymbol from "./brainwave-symbol.svg";
-import brainwaveWhiteSymbol from "./brainwave-symbol-white.svg";
+import edunovaSymbol from "./edunova-symbol.svg";
+import edunovaWhiteSymbol from "./edunova-symbol-white.svg";
 import play from "./play.svg";
 import gradient from "./gradient.png";
 import smallSphere from "./4-small.png";
@@ -76,12 +76,12 @@ import telegram from "./socials/telegram.svg";
 import twitter from "./socials/twitter.svg";
 
 export {
-  brainwave,
+  edunova,
   check,
   check2,
   loading1,
-  brainwaveSymbol,
-  brainwaveWhiteSymbol,
+  edunovaSymbol,
+  edunovaWhiteSymbol,
   play,
   gradient,
   smallSphere,
