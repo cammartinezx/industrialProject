618f821a096d7173a8c22b9fc4f748901c327b51
HungLu Dao <hungludao@gmail.com>
2025-03-06 21:51:39 -0600

    Update function to add student into course

--- back-end/src/Handler/CourseHandler.js
+++ back-end/src/Handler/CourseHandler.js
@@ -4,13 +4,30 @@ const { validateString } = require('../Utility/validator');
 class CourseHandler {
     
     #course_persistence;
+    #student_persistence;
 
     constructor() {
         this.#course_persistence = Services.get_course_persistence();
+        this.#student_persistence = Services.get_student_persistence();
     }
 
     get_course_persistence() {
         return this.#course_persistence;
+    }
+
+    async add_student(request, response) {
+        try {
+            const { student_id, course_id } = request.body;
+
+            if (!validateString(student_id) || !validateString(course_id)) {
+                return response.status(400).json({ message: "Invalid data" });
+            }
+
+            await this.#course_persistence.add_student(student_id, course_id);
+            response.status(200).json({ message: "Student added to course" });
+        } catch (error) {
+            response.status(500).json({ message: "Internal server error" });
+        }
     }
 
     async get_courses(request, response) {
--- back-end/src/Router/Course.js
+++ back-end/src/Router/Course.js
@@ -8,6 +8,10 @@ router.get('/get-courses', async (req, res) => {
     course_handler.get_courses(req, res);
 });
 
+router.post('/add-student', async (req, res) => {
+    course_handler.add_student(req, res);
+});
+
 router.use("/", (req, res) => {
     res.status(200).json({ Message: "Welcome to the Course paths" });
 });
