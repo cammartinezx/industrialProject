f9948cdc16eba6544c0b54bf891f4e9b2b85201c
Camila <camila.martinez2002@gmail.com>
2025-03-30 14:33:22 -0500

    Upload chatpdf to our s3

--- back-end/src/s3.js
+++ back-end/src/s3.js
@@ -1,39 +1,37 @@
-import dotenv from 'dotenv';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import { v4 as uuidv4 } from 'uuid';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-
-
+import dotenv from "dotenv";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 dotenv.config();
 
-const region = process.env.REGION;
-const bucketName = process.env.BUCKET_NAME;
-const accessKeyId = process.env.BUCKET_ACCESS_KEY_ID;
-const secretAccessKey = process.env.BUCKET_SECRET_ACCESS_KEY;
-
-const s3Client = new S3Client({
-  region,
+// 🔹 S3 Clients for Default & Student Buckets
+const defaultS3Client = new S3Client({
+  region: process.env.REGION,
   credentials: {
-    accessKeyId,
-    secretAccessKey,
+    accessKeyId: process.env.BUCKET_ACCESS_KEY_ID,
+    secretAccessKey: process.env.BUCKET_SECRET_ACCESS_KEY,
   },
 });
 
-export async function generateUploadURL(file_name) {
-  const fileName = file_name; // Unique file name
+const studentS3Client = new S3Client({
+  region: process.env.REGION,
+  credentials: {
+    accessKeyId: process.env.STUDENT_BUCKET_ACCESS_KEY_ID,
+    secretAccessKey: process.env.STUDENT_BUCKET_SECRET_ACCESS_KEY,
+  },
+});
+
+// 🔹 Generate Pre-Signed Upload URL
+export async function generateUploadURL(file_name, isStudent = false) {
+  const bucketName = isStudent ? process.env.STUDENT_BUCKET_NAME : process.env.BUCKET_NAME;
+  const s3Client = isStudent ? studentS3Client : defaultS3Client;
 
   const putObjectCommand = new PutObjectCommand({
     Bucket: bucketName,
-    Key: fileName,
-  })
+    Key: file_name,
+  });
 
-  const uploadURL = await getSignedUrl(
-    s3Client,
-    putObjectCommand,
-    { expiresIn: 60 } // 60 seconds
-  )
+  const uploadURL = await getSignedUrl(s3Client, putObjectCommand, { expiresIn: 60 });
 
-
-  return {uploadURL}
+  return { uploadURL };
 }
